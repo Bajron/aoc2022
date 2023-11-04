32b375ccd73bdb1e87e58546c64e5c9eef404bbc
btee <bkramarski@gmail.com>
2023-11-04 19:53:23 +0100

    Wrap main in try/catch block
    Not catching exceptions is UB.
    Also it makes better output on Windows.

--- src/bin/main.cpp
+++ src/bin/main.cpp
@@ -1,6 +1,18 @@
+#include <exception>
 #include <iostream>
 
-int main() {
+int _main() {
     std::cout << "Hello!" << std::endl;
     return 0;
+}
+
+int main() {
+    try {
+        return _main();
+    } catch (const std::exception& ex) {
+        std::cerr << " ** Exception: " << ex.what() << std::endl;
+    } catch (...) {
+        std::cerr << " ** Unknown exception" << std::endl;
+    }
+    return 1;
 }
