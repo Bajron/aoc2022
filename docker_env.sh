3ff6c83ba3c2b50f87363d7c2925648a5e3a9651
bkramarski <bkramarski@gmail.com>
2023-11-04 21:27:33 +0100

    Pass arguments to the docker call

--- docker_env.sh
+++ docker_env.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 HERE=$(dirname $(readlink -f "${BASH_SOURCE[0]}"))
 docker build -t gcc "${HERE}/docker"
-exec docker run -it --rm -u $(id -u):$(id -g) -v "${HERE}:${HERE}" -w "${HERE}" gcc
+exec docker run -it --rm -u $(id -u):$(id -g) -v "${HERE}:${HERE}" -w "${HERE}" gcc "${@}"
